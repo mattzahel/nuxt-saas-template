a0909f0bc91369c2260734c6a1510d64a21febf6
Mateusz Zahel <mateusz.zahel@gmail.com>
2024-04-20 17:22:27 +0200

    chore: add @nuxt/fonts @nuxt/image @nuxtjs/seo @vueuse/nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxtjs/seo',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@vueuse/nuxt' /* disabled from auto-import: toRefs, useFetch, useCookie, useHead, useTitle, useStorage */,
+  ],
+  site: {
+    url: 'https://example.com',
+    name: 'Awesome Site',
+    description: 'Welcome to my awesome site!',
+    defaultLocale: 'en' /* not needed if you have @nuxtjs/i18n installed */,
+  },
 });
